dc48b9c0a65d7e247662979c85583de231f20f34
florencia <mellone.flore@gmail.com>
2021-06-08 21:10:52 -0300

    first commit

---
+++ src/components/CarrouselHome.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "CarrouselHome"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Publicaciones.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Publicaciones"
+}
+</script>
+
+<style scoped>
+
+</style>
