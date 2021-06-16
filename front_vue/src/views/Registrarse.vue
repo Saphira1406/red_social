7614deff7e75c048d7727dc97c623d80ba6deaef
“erica_torrico” <ericatorrico@gmail.com>
2021-06-15 22:04:42 -0300

    Arreglando sobreescritura de Registrarse.vue

--- front_vue/src/views/Registrarse.vue
+++ front_vue/src/views/Registrarse.vue
@@ -1,26 +1,48 @@
 <template>
-  <section class="container-fluid d-flex justify-content-center align-items-center">
+  <section
+    class="container-fluid d-flex justify-content-center align-items-center"
+  >
     <div class="card text-white">
       <div class="card-header">
         <h2 class="card-title">Registrate</h2>
       </div>
       <div class="card-body">
-        <form action="#" class="row g-3">
+        <form action="#" class="row g-3" @submit.prevent="crearUsuario">
           <div class="col-md-6">
             <label for="nombre" class="form-label">Nombre</label>
-            <input type="text" class="form-control" id="nombre">
+            <input
+              type="text"
+              class="form-control"
+              id="nombre"
+              v-model="usuario.nombre"
+            />
           </div>
           <div class="col-md-6">
             <label for="apellido" class="form-label">Apellido</label>
-            <input type="text" class="form-control" id="apellido">
+            <input
+              type="text"
+              class="form-control"
+              id="apellido"
+              v-model="usuario.apellido"
+            />
           </div>
           <div class="col-12">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email">
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              v-model="usuario.email"
+            />
           </div>
           <div class="col-12">
             <label for="password" class="form-label">Contraseña</label>
-            <input type="password" class="form-control" id="password">
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              v-model="usuario.password"
+            />
           </div>
           <div class="d-grid gap-2 w-100 d-flex justify-content-center mx-auto">
             <button type="submit" class="btn boton mx-auto" >Registrarse</button>
@@ -29,12 +51,50 @@
       </div>
     </div>
   </section>
-
 </template>
 
 <script>
+import { apiFetch } from "../functions/fetch.js";
 export default {
-  name: "Registrarse"
+  name: "Registrarse",
+  data () {
+    return {
+      usuario: {
+        nombre: null,
+        apellido: null,
+        email: null,
+        password: null,
+      },
+    }
+  },
+  methods: {
+    crearUsuario () {
+      apiFetch('/registro.php', {
+        method: 'post',
+        body: JSON.stringify(this.usuario),
+      })
+        .then(rta => {
+          //  estado.classList.remove('d-none', 'alert-danger', 'alert-success');
+          console.log('respuesta del Post', rta);
+          /*
+                          estado.classList.add('alert');
+                          if (responseData.success) {
+                              estado.classList.add('alert-success');
+          
+                              setTimeout(
+                                  function () {
+                                      location.href = 'login.php';
+                                  }, 2000
+                              );
+                          } else {
+                              estado.classList.add('alert-danger');
+          
+                          }
+                          mostrarMensaje(responseData);
+                          */
+        });
+    }
+  },
 }
 </script>
 
