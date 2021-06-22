84882b56911596c29c2173e47239aa76468f6009
“erica_torrico” <ericatorrico@gmail.com>
2021-06-22 20:19:32 -0300

    Styling Profile section

--- front_vue/src/views/Perfil.vue
+++ front_vue/src/views/Perfil.vue
@@ -1,23 +1,27 @@
 <template>
-  <section class="profile">
-    <div class="text-center">
-      <img src="./../assets/img/persona_2.jpg" class="img-profile" alt="foto de perfil">
-      <p class="font-weight-bold">Nombre de Perfil</p>
-    </div>
+  <section class="profile text-center">
+    <img
+      src="./../assets/img/persona_2.jpg"
+      class="img-profile"
+      alt="foto de perfil"
+    />
+    <p class="font-weight-bold">Nombre de Perfil</p>
   </section>
 
-  <section class="info d-flex justify-content-center align-items-center flex-column">
+  <section
+    class="info d-flex justify-content-center align-items-center flex-column"
+  >
     <router-view></router-view>
     <div class="card mt-3 mb-3 card-info" style="width: 35rem;">
       <div class="card-header">
         <p class="h5">Información</p>
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item font-weight-bold">Nombre: </li>
-        <li class="list-group-item font-weight-bold">Apellido: </li>
-        <li class="list-group-item font-weight-bold">Email: </li>
-        <li class="list-group-item font-weight-bold">Fecha de Nacimiento: </li>
-        <li class="list-group-item font-weight-bold">Sexo: </li>
+        <li class="list-group-item font-weight-bold">Nombre:</li>
+        <li class="list-group-item font-weight-bold">Apellido:</li>
+        <li class="list-group-item font-weight-bold">Email:</li>
+        <li class="list-group-item font-weight-bold">Fecha de Nacimiento:</li>
+        <li class="list-group-item font-weight-bold">Sexo:</li>
       </ul>
     </div>
   </section>
@@ -25,19 +29,35 @@
 
 <script>
 export default {
-name: "Perfil"
+  name: "Perfil"
 }
 </script>
 
 <style scoped>
 .profile {
-  background: rgb(172,92,207);
-  background: linear-gradient(30deg, rgba(172,92,207,0.9107842966288078) 33%, rgba(242,107,143,0.90238093528427) 85%);
+  position: relative;
+  background: rgba(242, 137, 114, 0.1);
+  z-index: -1;
+}
 
+.profile:before {
+  position: absolute;
+  content: "";
+  width: 100%;
+  height: 37%;
+  top: 5em;
+  left: 0;
+  z-index: -1;
+  background: rgb(172, 92, 207);
+  background: linear-gradient(
+    30deg,
+    rgba(172, 92, 207, 0.9107842966288078) 33%,
+    rgba(242, 107, 143, 0.90238093528427) 85%
+  );
 }
 
 .info {
-  background: rgba(242,137,114, .1);
+  background: rgba(242, 137, 114, 0.1);
   padding-top: 2em;
   margin-top: -1em;
   padding-bottom: 1em;
@@ -45,16 +65,16 @@ name: "Perfil"
 }
 
 .card-info {
-  background-color: rgba(242,166,73, .5);
-  font-family: 'SourceSansPro', sans-serif;
+  background-color: rgba(242, 166, 73, 0.5);
+  font-family: "SourceSansPro", sans-serif;
 }
 
-.card-info ul li{
-  background-color: rgba(242,166,73, .1);
+.card-info ul li {
+  background-color: rgba(242, 166, 73, 0.1);
 }
 
 .img-profile {
-  width: 10%;
+  width: 170px;
   border-radius: 50%;
   margin-top: 6.5em;
 }
