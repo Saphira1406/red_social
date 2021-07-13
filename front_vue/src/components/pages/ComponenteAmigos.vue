41003f65bfa561be478edbaaac0e0e84f571f030
florencia <mellone.flore@gmail.com>
2021-07-12 21:57:41 -0300

    primer diseño de amigos

--- front_vue/src/components/pages/ComponenteAmigos.vue
+++ front_vue/src/components/pages/ComponenteAmigos.vue
@@ -1,7 +1,25 @@
 <template>
-  <section class="coming-soon">
-    <img src="./../../assets/img/soon.png"  class="img-fluid" alt="en construccion">
-  </section>
+  <article class="coming-soon">
+    <div class="card mb-3 d-flex align-items-center" style="max-width: 500px;">
+      <div class="row no-gutters align-items-center">
+        <div class="col-md-4 ml-3">
+          <img src="./../../assets/img/persona_2.jpg"  class="img-friend" alt="...">
+        </div>
+        <div class="col-md-7">
+          <div class="card-body">
+            <h5 class="card-title">Nombre Apellido</h5>
+            <p class="card-text">Usuario</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <button class="btn btn-outline-danger">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash2" viewBox="0 0 16 16">
+                <path d="M14 3a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2zM3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5c-1.954 0-3.69-.311-4.785-.793z"/>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </article>
 </template>
 
 <script>
@@ -11,8 +29,10 @@ export default {
 </script>
 
 <style scoped>
-.coming-soon {
-  background: none;
-  margin-bottom: 2em;
+
+.img-friend {
+  width: 75%;
+  border-radius: 50%;
 }
+
 </style>
