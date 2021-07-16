f81c94b59a9bd00b1152c50b25aa26e06f633f52
“erica_torrico” <ericatorrico@gmail.com>
2021-07-16 20:46:01 -0300

    first commit in new branch

--- front_vue/src/constants/api.js
+++ front_vue/src/constants/api.js
@@ -1,3 +1,2 @@
-export const API_HOST = 'http://localhost/davinci/programacion_3/parcial_2/proyecto/api/mvc/public';
-
+export const API_HOST = 'http://localhost/red_social/api/mvc/public';
 export const API_IMGS_FOLDER = API_HOST + '/img';
