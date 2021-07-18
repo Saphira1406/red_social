b88fee26c86752d455215c8ae2f5db3f8a2ebb06
florencia <mellone.flore@gmail.com>
2021-07-18 18:39:12 -0300

    boton agregar amigo

--- front_vue/src/components/UnaPublicacion.vue
+++ front_vue/src/components/UnaPublicacion.vue
@@ -16,6 +16,13 @@
           </p>
           <p class="small ml-3 mb-0">{{ publicacion.fecha }}</p>
         </div>
+        <div class="ml-auto">
+          <button class="btn btn-add">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
+          </button>
+        </div>
         <!-- Editar y eliminar publicación, todavía no agregado al backend:
         <div
           v-if="publicacion.usuarios_id == user.id"
