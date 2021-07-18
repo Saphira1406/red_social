c21e65b9fdbe7b888a2005215ff1678848c3bca7
florencia <mellone.flore@gmail.com>
2021-07-18 11:14:39 -0300

    bajando cambios

--- front_vue/src/components/pages/ComponenteAmigos.vue
+++ front_vue/src/components/pages/ComponenteAmigos.vue
@@ -1,80 +1,12 @@
 <template>
-  <article class="coming-soon">
-    <div class="card mb-3 d-flex align-items-center usuarios" style="max-width: 500px;">
-      <div class="row no-gutters align-items-center">
-        <div class="col-md-4 ml-3">
-          <img src="./../../assets/img/persona_2.jpg"  class="img-friend" alt="...">
-        </div>
-        <div class="col-md-7">
-          <div class="card-body d-flex justify-content-between">
-            <div>
-              <h5 class="card-title">Nombre Apellido</h5>
-              <p class="card-text">Usuario</p>
-              <p class="card-text">email@ejemplo.com</p>
-            </div>
-            <div class="align-self-center">
-              <button class="btn btn-outline-danger">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash2" viewBox="0 0 16 16">
-                  <path d="M14 3a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2zM3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5c-1.954 0-3.69-.311-4.785-.793z"/>
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-
-    <div class="card mb-3 d-flex align-items-center usuarios" style="max-width: 500px;">
-      <div class="row no-gutters align-items-center">
-        <div class="col-md-4 ml-3">
-          <img src="./../../assets/img/persona_3.jpg"  class="img-friend" alt="...">
-        </div>
-        <div class="col-md-7">
-          <div class="card-body d-flex justify-content-between">
-            <div>
-              <h5 class="card-title">Nombre Apellido</h5>
-              <p class="card-text">Usuario</p>
-              <p class="card-text">email@ejemplo.com</p>
-            </div>
-            <div class="align-self-center">
-              <button class="btn btn-outline-danger">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash2" viewBox="0 0 16 16">
-                  <path d="M14 3a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2zM3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5c-1.954 0-3.69-.311-4.785-.793z"/>
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="card mb-3 d-flex align-items-center usuarios" style="max-width: 500px;">
-      <div class="row no-gutters align-items-center">
-        <div class="col-md-4 ml-3">
-          <img src="./../../assets/img/persona_3.jpg"  class="img-friend" alt="...">
-        </div>
-        <div class="col-md-7">
-          <div class="card-body d-flex justify-content-between">
-            <div>
-              <h5 class="card-title">Nombre Apellido</h5>
-              <p class="card-text">Usuario</p>
-              <p class="card-text">email@ejemplo.com</p>
-            </div>
-            <div class="align-self-center">
-              <button class="btn btn-outline-danger">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash2" viewBox="0 0 16 16">
-                  <path d="M14 3a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2zM3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5c-1.954 0-3.69-.311-4.785-.793z"/>
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </article>
-
+  <section class="coming-soon">
+    <img
+      src="./../../assets/img/soon.png"
+      class="img-fluid"
+      alt="Sección en construcción"
+    />
+  </section>
+</template>
 
 <script>
 export default {
@@ -83,15 +15,8 @@ export default {
 </script>
 
 <style scoped>
-
-.usuarios {
-  background: rgb(242,137,114);
-  background: linear-gradient(94deg, rgba(242,137,114,0.7371148288416929) 21%, rgba(242,166,73,0.7735293946680235) 43%);
+.coming-soon {
+  background: none;
+  margin-bottom: 2em;
 }
-
-.img-friend {
-  width: 75%;
-  border-radius: 50%;
-}
-
 </style>
